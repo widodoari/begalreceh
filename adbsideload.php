b4a64c70bda0e7ca90eb50caf1d4f604dbb20827
Xplayer12 <65338756+Xplayer12@users.noreply.github.com>
2020-06-25 17:49:25 +0700

    Update adbsideload.php

--- adbsideload.php
+++ adbsideload.php
@@ -83,7 +83,7 @@ echo color("white","                  Format Kode 62*** \n");
         echo color("white",".");
         sleep(3);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2206"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2206"}');
         $message = fetch_value($code1,'"message":"','"');
         echo "\n".color("white"," Message: ".$message);
         echo "\n".color("white"," REFRESH..");
